3acaf44b0c62ade036628f95b58f400274354c54
Hugh Powell <hugh@hughpowell.co.uk>
2022-02-13 17:38:09 +1100

    Add git tagging ability to build

--- build.clj
+++ build.clj
@@ -1,12 +1,16 @@
 (ns build
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
-            [org.corfield.build :as bb]))
+            [org.corfield.build :as bb]
+            [clojure.java.shell :as shell]))
 
 (def lib 'hughpowell.co.uk/integrant-template)
-(def version "0.1.0-SNAPSHOT")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
+
 (def version (format "1.0.%s" (b/git-count-revs nil)))
+
+(defn tag [message & _]
+  (shell/sh "git" "tag" "-a" version "-m" message)
+  (shell/sh "git" "push" "--tags"))
 
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
